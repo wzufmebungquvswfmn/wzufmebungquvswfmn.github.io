cc09477fd0c27c57bfbac7907166caaa106ebaf8
wzufmebungquvswfmn <1961414324@qq.com>
2026-05-04 13:40:42 +0800

    update

--- docs/notes/searcher-c2a407aa.js
+++ docs/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a0c01507.js',
+                path_to_root + 'searchindex-e5c1d767.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
