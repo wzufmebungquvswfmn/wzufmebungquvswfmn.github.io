8c5f1daa86cffc4ae17437b8da81bbbd11f5cfc5
wzufmebungquvswfmn <1961414324@qq.com>
2026-05-09 00:20:36 +0800

    update

--- docs/notes/searcher-c2a407aa.js
+++ docs/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d86b7fd4.js',
+                path_to_root + 'searchindex-5ae99f90.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
